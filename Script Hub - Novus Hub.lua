bea9d2c472b5404a221e3d9eace4caca748f68e9
kxzyInt <113261037+kxzyInt@users.noreply.github.com>
2023-02-12 10:54:10 +0100

    Update Script Hub - Novus Hub.lua

--- Script Hub - Novus Hub.lua
+++ Script Hub - Novus Hub.lua
@@ -3,7 +3,7 @@ local Games = {
 	[10925589760] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Merge%20Simulator.lua", -- Merge Simulator
 	[10404327868] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Timber%20Champions.lua", -- Timber Champions
 	[11445923563] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/One%20Fruit%20Simulator.lua", -- One Fruit Simulator
-	[10821317529] = "https://raw.githubusercontent.com/alyssagithub/Scripts/main/Inferno%20X%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua" -- Pickaxe Mining Simulator
+	[10821317529] = "https://raw.githubusercontent.com/kxzyInt/Scripts/main/Novus%20Hub%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua" -- Pickaxe Mining Simulator
 }
 
 if Games[game.PlaceId] then
