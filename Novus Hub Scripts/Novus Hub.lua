55e8341afea6966b471835a831461d0723420590
kxzyInt <113261037+kxzyInt@users.noreply.github.com>
2023-02-12 15:00:40 +0100

    Update Novus Hub.lua

--- Novus Hub Scripts/Novus Hub.lua
+++ Novus Hub Scripts/Novus Hub.lua
@@ -1,5 +1,5 @@
 local Games = 
-	[10821317529] = "https://raw.githubusercontent.com/kxzyInt/Scripts/main/Novus%20Hub%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua" -- Pickaxe Mining Simulator
+	[10821317529] = "https://raw.githubusercontent.com/kxzyInt/Scripts/main/Novus%20Hub%20Scripts/Games/Pickaxe%20Mining%20Sim.lua" -- Pickaxe Mining Simulator
 }
 
 if Games[game.PlaceId] then
