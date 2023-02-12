3d8752a46f8008bf7e3aff7638ac0bc2ed0526d0
kxzyInt <113261037+kxzyInt@users.noreply.github.com>
2023-02-12 14:48:39 +0100

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,13 @@
+local Games = 
+	[10821317529] = "https://raw.githubusercontent.com/kxzyInt/Scripts/main/Novus%20Hub%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua" -- Pickaxe Mining Simulator
+}
+
+if Games[game.PlaceId] then
+	loadstring(game:HttpGet(Games[game.PlaceId]))()
+else
+	game.StarterGui:SetCore("SendNotification", {
+	    Title = "Novus Hub Notification";
+	    Text = "Game not supported";
+	    Duration = 10;
+    })
+end
